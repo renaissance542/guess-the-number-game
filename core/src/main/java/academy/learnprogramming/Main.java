cad6d1a4b069f87b90b11844630cf6d92ded3615
philipperoy <renaissance542@gmail.com>
2020-11-02 17:52:44 -0500

    Game and NumberGenerator interfaces and implementations added

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -2,12 +2,51 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     
     private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final String CONFIG_LOCATION = "beans.xml";
+    
     public static void main(String[] args) {
         log.info("Guess the Number Game");
+        
+        // create context (container)
+        ConfigurableApplicationContext context =
+                new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        
+        // get numberGenerator bean from context (container)
+        NumberGenerator numberGenerator
+                = context.getBean("numberGenerator", NumberGenerator.class);
+        
+        // call method next() to get a random number
+        int number = numberGenerator.next();
+        
+        // log generated number
+        log.info("number = {}", number);
+        
+        // get game bean from context (container)
+        Game game = context.getBean("game", Game.class);
+        
+        // close the context (container)
+        context.close();
+        
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
