9a36074f94150af1dcf7446d74659c29e813836e
philipperoy <renaissance542@gmail.com>
2021-02-25 16:51:00 -0500

    Abstracted the attribute names in the thymeleaf files.  This broke with the newer version of Thymeleaf because ${__${}__} is no longer allowed to call static methods. The constants were added as attributes in the controller. A game-over view was added along with controller functionality.
    Signed-off-by: philipperoy <renaissance542@gmail.com>

--- web/src/main/java/academy/learnprogramming/controller/GameController.java
+++ web/src/main/java/academy/learnprogramming/controller/GameController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -29,15 +31,40 @@ public class GameController {
     public String play(Model model) {
         model.addAttribute(AttributeNames.MAIN_MESSAGE, gameService.getMainMessage());
         model.addAttribute(AttributeNames.RESULT_MESSAGE, gameService.getResultMessage());
+        model.addAttribute("PLAY", ViewNames.PLAY);
+        model.addAttribute("MAIN_MESSAGE", AttributeNames.MAIN_MESSAGE);
+        model.addAttribute("RESULT_MESSAGE", AttributeNames.RESULT_MESSAGE);
         log.info("model = {}", model);
         if (gameService.isGameOver()) {
-            return ViewNames.GAME_OVER;
+            return "redirect:/" + ViewNames.GAME_OVER;
         } else {
             return ViewNames.PLAY;
         }
     }
     
+    @PostMapping(GameMappings.PLAY)
+    public String processMessage(@RequestParam int guess) {
+        gameService.checkGuess(guess);
+        log.info("guess = {}", guess);
+        return GameMappings.REDIRECT_PLAY;
+    }
+    
+    @GetMapping(GameMappings.GAME_OVER)
+    public String gameOver(Model model) {
+        model.addAttribute(AttributeNames.RESULT_MESSAGE, gameService.getResultMessage());
+        model.addAttribute("RESULT_MESSAGE", AttributeNames.RESULT_MESSAGE);
+        model.addAttribute("PLAY", ViewNames.PLAY);
+        gameService.reset();
+        log.info("gameService.reset() was called");
+        return ViewNames.GAME_OVER;
+    }
 }
+
+
+
+
+
+
 
 
 
--- web/src/main/java/academy/learnprogramming/util/GameMappings.java
+++ web/src/main/java/academy/learnprogramming/util/GameMappings.java
@@ -5,6 +5,7 @@ public final class GameMappings {
     // == constants ==
     public static final String PLAY = "play";
     public static final String REDIRECT_PLAY = "redirect:/" + PLAY;
+    public static final String GAME_OVER = "game-over";
     
     // == constructor ==
     private GameMappings() {
